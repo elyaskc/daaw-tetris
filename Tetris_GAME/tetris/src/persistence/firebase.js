385310e382067302cda594629ca128260604e9d7
elyaskc <elyas8e@gmail.com>
2026-03-15 15:44:44 +0100

    Security changes

--- Tetris_GAME/tetris/src/persistence/firebase.js
+++ Tetris_GAME/tetris/src/persistence/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqP88f1sxdBMLKeujWVZGNOZaEhl1vtzs",
-  authDomain: "daaw-tetris.firebaseapp.com",
-  projectId: "daaw-tetris",
-  storageBucket: "daaw-tetris.firebasestorage.app",
-  messagingSenderId: "602454793912",
-  appId: "1:602454793912:web:fd481f322d5ca14d1c1e4d",
-  measurementId: "G-VPQH884G9S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app, "https://daaw-tetris-default-rtdb.firebaseio.com/");
+// Usamos la URL desde el .env también
+export const db = getDatabase(app, import.meta.env.VITE_FIREBASE_DATABASE_URL);
